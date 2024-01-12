89261f66b3633524895b5c4142d24af3297249ac
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2024-01-12 22:28:20 +0530

    Update recursiveApproach.java

--- Day 1/1. Check if a String is Pallindrome/recursiveApproach.java
+++ Day 1/1. Check if a String is Pallindrome/recursiveApproach.java
@@ -5,6 +5,7 @@ public class recursiveApproach {
         Scanner read = new Scanner(System.in);
         String inpuString = read.next();
         read.close();
+        //fetching the length of the string
         int length = inpuString.length();
         int checkPointer = 0;
         boolean result = checkPallindrome(checkPointer,inpuString, length);
@@ -12,15 +13,18 @@ public class recursiveApproach {
     }
 
     private static boolean checkPallindrome(int checkPointer, String inputString, int length) {
-        
+        //checking weather we reach till half of the string or not
+        //base condition for recursion
         if (checkPointer >= length/2)
         {
             return true;
         }
+        //checking characters from the starting and the end of the string
         if (inputString.charAt(checkPointer) != inputString.charAt(length - checkPointer-1 ))
         {
             return false;
         }
+        //again calling same function => recursion
         return checkPallindrome(checkPointer+1, inputString, length);
     }
 }
