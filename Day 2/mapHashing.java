79d33ceaba2c96cd12e5f447eabd8cc9706af87d
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2024-01-20 21:32:31 +0530

    Add files via upload

---
+++ Day 2/mapHashing.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+
+import java.util.Scanner;
+public class mapHashing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+
+        }
+
+        //precompute
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < n; i ++)
+        {
+            int key = arr[i];
+            int freq = 0;
+            if (mp.containsKey(key)) 
+            freq = mp.get(key); // fetching from the map
+            freq++;
+            mp.put(key, freq); //Inserting into the map
+        }
+
+        //Iterate over the map
+        /*
+           for (Map.Entry<Integr, Integer> it : mp.entrySet()) {
+            System.out.println(it.getKey() + "->" + it.getValue());
+           }
+         */
+
+        int q;
+        q = sc.nextInt();
+        while (q-- > 0)
+        {
+            int number; 
+            number = sc.nextInt();
+            //fetch
+            if (mp.containsKey(number))
+            System.out.println(mp.get(number));
+            else
+            System.out.println(0);
+        }
+        sc.close();
+    }
+}
